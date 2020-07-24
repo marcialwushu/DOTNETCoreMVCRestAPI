5a15f00c23a51687d2f1ae8cce43c8bc45705176
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-07-24 15:55:14 -0300

    Model Command create

---
+++ Models/Command.cs
@@ -0,0 +1,12 @@
+namespace Command.using System;
+
+namespace Commander.Models.Models
+{
+    public class Command
+    {
+        public Command()
+        {
+            Console.WriteLine("Empty Class");
+        }
+    }
+}

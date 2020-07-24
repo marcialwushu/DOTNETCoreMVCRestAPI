7b5e751184601de8209733ef8be94057ff448885
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-07-24 16:54:39 -0300

    CommandController init

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommanderController : ControllerBase
+    {
+        
+    }
+}
